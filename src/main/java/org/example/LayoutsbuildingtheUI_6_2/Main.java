aadfbc1c0ef4f03ee268eeaf9ba2836fb8a399e7
dornarajK <devraj.kharal05@gmail.com>
2026-04-21 16:49:32 +0300

    commit

---
+++ src/main/java/org/example/LayoutsbuildingtheUI_6_2/Main.java
@@ -0,0 +1,17 @@
+package org.example.LayoutsbuildingtheUI_6_2;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello JavaFX");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
